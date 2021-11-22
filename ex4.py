62b40a9e735d4f6fe9c92fd70afb941387cb3aa5
ArnauGonzalez-upf <77241003+ArnauGonzalez-upf@users.noreply.github.com>
2021-11-22 11:44:21 +0100

    Delete ex4.py

--- ex4.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    #ffmpeg -i BBB_CUT.mp4 -ac 1  BBB_CUT.flac
-    #ffmpeg -i input_filename.avi -acodec mp3 -vcodec copy output_filename.avi
-    cmd = 'ffmpeg -i BBB_CUT.mp4 -ac 1 -acodec mp3 -vcodec copy BBB_CUT_MONO.mp4'
-    os.system(cmd)
